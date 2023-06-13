22b2155feb652e903657e79f7162591f6aeafabb
Sergei Kolinichenko <iamserj61@gmail.com>
2023-06-13 12:50:50 +0300

    start

---
+++ app/src/main/java/info/sergeikolinichenko/App.kt
@@ -0,0 +1,12 @@
+package info.sergeikolinichenko
+
+import android.app.Application
+
+/** Created by Sergei Kolinichenko on 13.06.2023 at 12:45 (GMT+3) **/
+
+class App: Application() {
+
+  override fun onCreate() {
+    super.onCreate()
+  }
+}
